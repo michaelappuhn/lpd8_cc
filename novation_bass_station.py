e32de0a450923f1384ec21aa66758c5ce6724c4b
Mikey Appuhn <mike.appuhn@gmail.com>
2024-08-25 20:16:12 -0700

    beginnings of mappings for bassstation

---
+++ novation_bass_station.py
@@ -0,0 +1,23 @@
+# Mod wheel - 1
+# Breath control - 2
+# Volume - 7
+
+# Frequency - 105
+# Resonance - 106
+# Mod Depth - 107
+
+# Env 1 Attack - 108
+# Env 1 Decay - 109
+# Env 1 Sustain - 110
+# Env 1 Release - 111
+# Env 1 Velocity - 112
+
+# Env 2 Attack - 114
+# Env 2 Decay - 115
+# Env 2 Sustain - 116
+# Env 2 Release - 117
+# Env 2 Velocity - 118
+
+mapping = {
+
+}
